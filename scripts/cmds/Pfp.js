5e6fb51fa0c8a13cd390ec8413260231e68662e3
BotTanvir <ahmeddipto123132@gmail.com>
2025-02-05 22:34:52 +0600

    Update Pfp.js

--- scripts/cmds/Pfp.js
+++ scripts/cmds/Pfp.js
@@ -5,7 +5,7 @@ module.exports = {
     version: "1.1",
     author: "NIB | No prefix by ArYan",
     countDown: 5,
-    role: 0,
+    role: 2,
     shortDescription: "PROFILE image",
     longDescription: "PROFILE image",
     category: "image",
